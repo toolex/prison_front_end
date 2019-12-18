7e680f375069136357e54c2e362d236d72d3ae0e
Stephen Boyle <1nobad@gmail.com>
2019-12-18 21:19:14 +0000

    some further tweaks to CSS

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div>
-      <h1>HMRC CodeClan</h1>
+      <h1 className = 'header'>HMP CodeClan</h1>
         <PrisonContainer/>
       </div>
     );
--- src/components/CellComponent.js
+++ src/components/CellComponent.js
@@ -1,6 +1,7 @@
 import React, {Fragment} from 'react';
 import PrisonerComponent from './PrisonerComponent';
 
+
 const CellComponent = (props) => {
   if(props.prisons.length === 0){
     return (<p>Loading...</p>)
@@ -16,7 +17,7 @@ const CellComponent = (props) => {
         <Fragment>
         <th></th><br></br>
         <th></th><th></th>
-        <th className = 'cellNumber' >Cell {cell.id}</th><th><button className = 'deleteCell' type="submit" value={cell.id} onClick={props.onCellDelete}> Delete Cell</button></th>
+        <th className = 'cellNumber' >Cell {cell.id}</th><th><button className = 'deleteCell' type="submit" value={cell.id} onClick={props.onCellDelete}>Delete Cell</button></th>
         <h1 key={index} className = 'cellBox'>
         <PrisonerComponent onHandleMovePrisoner={props.onHandleMovePrisoner} onPrisonerDelete={props.onPrisonerDelete} prisons={props.prisons} cell={cell} handleFeedPrisoner={props.handleFeedPrisoner}/>
         </h1>
